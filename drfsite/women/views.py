984a51ff545dbe37844559b87d17065cb6610297
teo <teodantes@gmail.com>
2022-06-23 22:56:49 +0300

    БАЗОВЫЙ КЛАСС APIVIEW ДЛЯ ПРЕДСТАВЛЕНИЙ

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
-from rest_framework import generics
+from django.forms import model_to_dict
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Women
-from .serializers import WomenSerializer
+# Этот класс стоит во главе иерархии всех классов представления DRF.
+# Самый базовый функционал.
 
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+class WomenAPIView(APIView):
+    def get(self, request):
+        # Метод, который будет отвечать за обработку GET-запросов.
+        lst = Women.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+
+        return Response({'post': model_to_dict(post_new)})
