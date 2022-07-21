e528cf21a48f742cce64a82477b91a4dc75774dd
teo <teodantes@gmail.com>
2022-07-21 22:51:24 +0300

    ВВЕДЕНИЕ В СЕРИАЛИЗАЦИЮ /

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,26 +1,28 @@
-from django.forms import model_to_dict
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Women
-# Этот класс стоит во главе иерархии всех классов представления DRF.
-# Самый базовый функционал.
+from .serializers import WomenSerializer
+
 
 # class WomenAPIView(generics.ListAPIView):
 #     queryset = Women.objects.all()
 #     serializer_class = WomenSerializer
 
+
 class WomenAPIView(APIView):
     def get(self, request):
-        # Метод, который будет отвечать за обработку GET-запросов.
-        lst = Women.objects.all().values()
-        return Response({'posts': list(lst)})
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
 
     def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         post_new = Women.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             cat_id=request.data['cat_id']
         )
 
-        return Response({'post': model_to_dict(post_new)})
+        return Response({'post': WomenSerializer(post_new).data})
